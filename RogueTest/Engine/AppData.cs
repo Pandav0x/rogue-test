6e5e6b8feef7e6005d1be1d3d3ea1e4db577a9b8
Pandav0x <pandav0x@hotmail.fr>
2018-01-13 11:23:11 +0100

    Update Version

--- RogueTest/Engine/AppData.cs
+++ RogueTest/Engine/AppData.cs
@@ -11,8 +11,8 @@ namespace RogueTest.Engine
         public static string name = "P.U.L.S.E Engine";
         public static string nameFull = "Panda Unlimited Low Spec Experience Engine";
 
-        public static string version = "0.1.0";
-        public static string buildName = "El Bridget (Freezing)";
+        public static string version = "0.1.1";
+        public static string buildName = "El Bridget";
 
         public static string buildDate = DateTime.Now.Day.ToString() + "_" + DateTime.Now.Month.ToString() + "_"+ DateTime.Now.Year.ToString();
      
